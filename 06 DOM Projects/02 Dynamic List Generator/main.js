85e6d852052a0d62c8092c9ae81d5a152cf88964
DevDivyansh <576divyanshsharma@gmail.com>
2026-03-19 19:25:25 +0530

    Dynamic List Generator version 1.1.1.1

--- 06 DOM Projects/02 Dynamic List Generator/main.js
+++ 06 DOM Projects/02 Dynamic List Generator/main.js
@@ -1,6 +1,6 @@
 // const input = document.getElementById("itemInput");
 
-const input = document.getElementById("itemInput");
+const getinput = document.getElementById("itemInput");
 
 const addBtn = document.getElementById("addBtn")
 
@@ -8,10 +8,79 @@ const itemList = document.getElementById("list")
 
 addBtn.addEventListener('click',()=>{
 
-    const newLi = document.createElement("li")
-    newLi.innerText=input.value 
-    itemList.append(newLi)
-    input.value=""
+    if(getinput.value.trim().length!==0){
+    
+    const li = document.createElement("li");
+
+    let span = document.createElement("span")
+    span.textContent = getinput.value
+    li.append(span)
+    
+    
+
+    const btnDiv = document.createElement("div")
+
+    const editBtn = document.createElement("button")
+
+    btnDiv.classList.add("btnDivison")
+
+    editBtn.classList.add("edit")
+
+    editBtn.textContent="Edit"
+    btnDiv.appendChild(editBtn)
+    const delButton = document.createElement("button");
+    delButton.textContent="Delete"
+    delButton.classList.add("delete");
+    btnDiv.appendChild(delButton);
+    li.appendChild(btnDiv)
+    itemList.appendChild(li);
+
+    delButton.addEventListener("click", () => {
+      li.remove(); // we can also Use itemList.removeChild(li)
+    });
+    
+
+    function changeData() {
+      const editData = document.createElement("div");
+
+      editData.classList.add("input-box");
+
+      const editText = document.createElement("input");
+
+      editText.value = span.textContent;
+
+      editData.append(editText);
+
+      const saveBtn = document.createElement("button");
+
+      saveBtn.textContent = "Save";
+      editBtn.replaceWith(saveBtn);
+      editData.append(btnDiv);
+
+      saveBtn.addEventListener("click", () => {
+        li.innerText = editText.value;
+        saveBtn.replaceWith(editBtn);
+        li.append(btnDiv);
+        editData.replaceWith(li);
+      });
+
+      itemList.replaceChild(editData, li);
+
+      getinput.value = "";
+    }
+     
+    li.addEventListener('dblclick',changeData)
+
+    editBtn.addEventListener('click',changeData)
+
+
+
+     getinput.value="";
+    
+    }
+
+
+    
 
      // Version 1
     // if(input.value.trim().length!==0){
