8d34fd3ace24395e1df906dc7ab849093dbeacce
DevDivyansh <576divyanshsharma@gmail.com>
2026-03-29 02:33:05 +0530

    Auth module logindto setup and authenticate and authorization middleware setup

--- 08 Real Backend/src/modules/auth/auth.routes.js
+++ 08 Real Backend/src/modules/auth/auth.routes.js
@@ -5,9 +5,16 @@ import * as controller from "./auth.controller.js"
 import validate from "../../common/middleware/validate.middleware.js"
 
 import RegisterDto from "./dto/register.dto";
+import { authenticate , authorize } from "./auth.middleware.js";
 
 const router = Router()
 
 router.post("/register",validate(RegisterDto),controller.register)
+
+router.post("/login",validate(LoginDto),controller.login)
+
+router.get("/me",authenticate,controller.getMe);
+
+// router.get("/verify-email/:token",controller.getMe);
 
 export default router;
