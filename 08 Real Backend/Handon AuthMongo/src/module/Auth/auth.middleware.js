28a9432ac640f2bb37370b479bf26cfd0d108dff
DevDivyansh <576divyanshsharma@gmail.com>
2026-04-02 17:44:34 +0530

    authorize

--- 08 Real Backend/Handon AuthMongo/src/module/Auth/auth.middleware.js
+++ 08 Real Backend/Handon AuthMongo/src/module/Auth/auth.middleware.js
@@ -20,9 +20,20 @@ const authenticate = async (req,res,next)=>{
         name:user.name,
         email:user.email
     };
-    
+
     next();
 
 }
 
-export {authenticate}
+const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw ApiError.forbidden(
+        "You do not have permission to perform this action",
+      );
+    }
+    next();
+  };
+};
+
+export {authenticate , authorize}
