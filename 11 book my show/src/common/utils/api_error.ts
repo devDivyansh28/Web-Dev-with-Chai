8928496cd01f6877e6531194c4bcbd7a99d7cc4c
DevDivyansh <576divyanshsharma@gmail.com>
2026-04-09 01:05:35 +0530

    Book My Show Module - Auht  => Update - api_error.ts

--- 11 book my show/src/common/utils/api_error.ts
+++ 11 book my show/src/common/utils/api_error.ts
@@ -1,16 +1,31 @@
-
-
 class ApiError extends Error {
+  statusCode: number;
+  isOperational: boolean;
 
+  constructor(statusCode: number, message: string) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
+  }
 
-    statusCode : number
-    isOperational : boolean
+  static badRequest(message = "Bad request") {
+    return new ApiError(400, message);
+  }
 
-    constructor(statusCode :number , message : string){
-        super(message);
-        this.statusCode  = statusCode;
-        this.isOperational = true;
-        Error.captureStackTrace(this,this.constructor);
-    }
+  static unauthorized(message = "Unauthorized") {
+    return new ApiError(401, message);
+  }
 
+  static conflict(message = "Conflict") {
+    return new ApiError(409, message);
+  }
+  static forbidden(message = "forbidden") {
+    return new ApiError(412, message);
+  }
+  static notfound(message = "notfound") {
+    return new ApiError(412, message);
+  }
 }
+
+export default ApiError;
