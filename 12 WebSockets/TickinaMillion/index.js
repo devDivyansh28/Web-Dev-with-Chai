3160af4ba720bd486eb3f21d936011aedc6b29fa
DevDivyansh <576divyanshsharma@gmail.com>
2026-04-27 02:11:34 +0530

    Completed True raw 1 Million CheckBoxes

--- 12 WebSockets/TickinaMillion/index.js
+++ 12 WebSockets/TickinaMillion/index.js
@@ -11,70 +11,152 @@ const CHECKBOX_STATE_KEY = 'checkbox-state'
 
 
 
-async function main(){
-    const PORT = process.env.PORT ?? 8000;
+async function main() {
+  const PORT = process.env.PORT ?? 8000;
 
-    // Server Part
-    const app = express();
-    const server = http.createServer(app);
-    const io = new Server();
-    io.attach(server);
+  // Server Part
+  const app = express();
+  const server = http.createServer(app);
+  const io = new Server();
+  io.attach(server);
 
-    await subscriber.subscribe('internal-server:checkbox:change');
+  await subscriber.subscribe("internal-server:checkbox:change");
 
-    subscriber.on('message',(channel , message)=>{
-        if(channel==='internal-server:checkbox:change'){
-            const {index , checked} = JSON.parse(message);
+//   subscriber.on("message", (channel, message) => {
+//     if (channel === "internal-server:checkbox:change") {
+//       const { index, checked } = JSON.parse(message);
 
-            io.emit('server:checkbox:change', {index , checked});
-        }
-    })
+//       io.emit("server:checkbox:change", { index, checked });
+//     }
+//   });
 
-    // Sockt IO Handlers
-    io.on('connection',(socket)=>{
-        console.log("Socket Connected" , socket.id);
+  const rateLimitingHashMap = new Map();
 
-        socket.on('client:checkbox:change', async (data)=>{
+  subscriber.on("message", async (channel, message) => {
 
-         const existingState = await redis.get(CHECKBOX_STATE_KEY)
+    if (channel === "internal-server:checkbox:change") {
+      const { index, checked } = JSON.parse(message);
 
-         if(existingState){
-            const remoteData =  JSON.parse(existingState);
-            remoteData[data.index] = data.checked;
-            redis.set(CHECKBOX_STATE_KEY , JSON.stringify(remoteData))
-         } else{
-              await redis.set(CHECKBOX_STATE_KEY , JSON.stringify(new Array(CHECKBOX_SIZE).fill(false)))
-         }
+      const remoteData = await getState();
+      remoteData[index] = checked;
+      await setState(remoteData);
+      io.emit("server:checkbox:change", { index, checked });
+    }
+  });
 
 
-        console.log(`${socket.id} : make a change`, data);
-        
-        await publisher.publish('internal-server:checkbox:change', JSON.stringify(data))
-        })
-    })
+   async function getState(){
+     const existingState = await redis.get(CHECKBOX_STATE_KEY);
+     const remoteData = JSON.parse(existingState);
+     return remoteData;
+   }
 
+   async function setState(remoteData){
+     await redis.set(CHECKBOX_STATE_KEY, JSON.stringify(remoteData));
+   }
+  // // Sockt IO Handlers
+  // io.on('connection',(socket)=>{
+  //     console.log("Socket Connected" , socket.id);
 
-    // Expresss
-    app.use(express.static(path.resolve('./public')));
-    app.get('/health',(req,res)=>{
-        res.json({helath : "ok"});
-    })
+  //     socket.on('client:checkbox:change', async (data)=>{
 
-    app.get('/checkboxes',async(req,res)=>{
+  //      const existingState = await redis.get(CHECKBOX_STATE_KEY)
 
-        const existingState = await redis.get(CHECKBOX_STATE_KEY);
-        if(existingState){
-            const remoteData = JSON.parse(existingState);
-            return res.json({checkboxes : remoteData})
-        }
+  //      if(existingState){
+  //         const remoteData =  JSON.parse(existingState);
+  //         remoteData[data.index] = data.checked;
+  //         redis.set(CHECKBOX_STATE_KEY , JSON.stringify(remoteData))
+  //      } else{
+  //           await redis.set(CHECKBOX_STATE_KEY , JSON.stringify(new Array(CHECKBOX_SIZE).fill(false)))
+  //      }
 
-        return res.json({ checkboxes: new Array(CHECKBOX_SIZE).fill(false) });
+  //     console.log(`${socket.id} : make a change`, data);
 
-    })
+  //     await publisher.publish('internal-server:checkbox:change', JSON.stringify(data))
+  //     })
+  // })
 
-    server.listen(PORT,()=>{
-        console.log(`Server is running on http://localhost:${PORT}`);
-    })
+  // Sockt IO Handlers
+  io.on("connection", (socket) => {
+    console.log("Socket Connected", socket.id);
+
+    socket.on("client:checkbox:change", async (data) => {
+     
+
+    const lastOperationTime = await redis.get(`rate-limit-${socket.id}`);
+    // const lastOperaitonTime = rateLimitingHashMap.get(socket.id);
+    if(lastOperaitonTime){
+          const timeElapsed = Date.now()-lastOperaitonTime;
+          if(timeElapsed < 5.5 *1000){
+            socket.emit('server:error',{error : 'Please Wait'})
+            return;
+          }
+    }
+    await redis.set(`rate-limit-${socket.id}`, Date.now());
+    //   const existingState = await redis.get(CHECKBOX_STATE_KEY);
+
+    //   if (existingState) {
+    //     const remoteData = JSON.parse(existingState);
+    //     remoteData[data.index] = data.checked;
+    //     redis.set(CHECKBOX_STATE_KEY, JSON.stringify(remoteData));
+    //   } else {
+    //     await redis.set(
+    //       CHECKBOX_STATE_KEY,
+    //       JSON.stringify(new Array(CHECKBOX_SIZE).fill(false)),
+    //     );
+    //   }
+
+      console.log(`${socket.id} : make a change`, data);
+
+      await publisher.publish(
+        "internal-server:checkbox:change",
+        JSON.stringify(data),
+      );
+    });
+  });
+
+  // Expresss
+  app.use(express.static(path.resolve("./public")));
+  app.get("/health", (req, res) => {
+    res.json({ helath: "ok" });
+  });
+
+  // app.get('/checkboxes',async(req,res)=>{
+
+  //     const existingState = await redis.get(CHECKBOX_STATE_KEY);
+  //     if(existingState){
+  //         const remoteData = JSON.parse(existingState);
+  //         return res.json({checkboxes : remoteData})
+  //     }
+
+  //     return res.json({ checkboxes: new Array(CHECKBOX_SIZE).fill(false) });
+
+  // })
+
+  app.get("/checkboxes", async (req, res) => {
+    try {
+      const remoteData = await getState();
+
+      if (remoteData) {
+        return res.json({ checkboxes: remoteData });
+      }
+      
+      const initial = new Array(CHECKBOX_SIZE).fill(false);
+      await setState(initial);
+      return res.json({ checkboxes: initial });
+    } catch (err) {
+      console.error(err);
+
+      // fallback
+      return res.json({
+        checkboxes: new Array(CHECKBOX_SIZE).fill(false),
+      });
+    }
+  });
+
+  server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 }
 
 main();
