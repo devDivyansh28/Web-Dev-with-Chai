7cee233fe3a6637fee76a08983a4695fdc6697f5
DevDivyansh <576divyanshsharma@gmail.com>
2026-05-30 01:17:21 +0530

    feat : nextjs framework

---
+++ 16 nextjs/chai-aur-nextjs/app/page.tsx
@@ -0,0 +1,8 @@
+import { TodoApp } from "@/components/todos/todo-app";
+import { fetchTodos } from "@/lib/todos";
+
+export default async function Home() {
+  const todos = await fetchTodos();
+
+  return <TodoApp initialTodos={todos} />;
+}
---
+++ 16 nextjs/chai-aur-nextjs/components/button.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from 'react'
+
+export const Button = () => {
+  return (
+    <button onClick={() => alert('Hello World')}>
+        Click me
+    </button>
+  )
+}
