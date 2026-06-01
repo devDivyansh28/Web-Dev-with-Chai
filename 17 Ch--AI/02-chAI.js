cfcfce83f45d6fe1fe1d53beaf6d196f57e2ed2f
DevDivyansh <576divyanshsharma@gmail.com>
2026-06-01 17:52:54 +0530

    feat: AI Introduction and startout

--- 17 Ch--AI/02-chAI.js
+++ 17 Ch--AI/02-chAI.js
@@ -1,36 +1,42 @@
-// import { checkOpenAI } from "./01-chAI.js";
+import { checkOpenAI } from "./01-chAI.js";
 
-// const client = await checkOpenAI();
-// const model = "gpt-4o-mini";
+const client = await checkOpenAI();
+const model = "gpt-4o-mini";
 
-// console.log(client.baseURL);
 
-// const role_anime =
-//   "You are a fan and love to talk about anime. You are very enthusiastic and always want to share your knowledge about anime with others.";
+console.log(client)
+console.log(client.baseURL);
 
-// const role_oogway =
-//   "You are Master Oogway, a wise and ancient turtle from the Kung Fu Panda universe. You speak in a calm and philosophical manner, often sharing profound insights and life lessons. Your responses are filled with wisdom and a touch of humor.";
+const role_anime =
+  "You are a fan and love to talk about anime. You are very enthusiastic and always want to share your knowledge about anime with others.";
 
-// const response = await client.chat.completions.create({
-//   model,
-//   messages: [
-//     {
-//       role: "system",
-//       content: role_oogway,
-//     },
-//     {
-//       role: "user",
-//       content: "Where should I travel in the world?",
-//     },
-//   ],
-// });
+const role_oogway =
+  "You are Master Oogway, a wise and ancient turtle from the Kung Fu Panda universe. You speak in a calm and philosophical manner, often sharing profound insights and life lessons. Your responses are filled with wisdom and a touch of humor.";
 
-// console.log(response.choices[0].message.content);
+const response = await client.chat.completions.create({
+  model,
+  messages: [
+    {
+      role: "system",
+      content: role_oogway,
+    },
+    {
+      role: "user",
+      content: "Where should I travel in the world?",
+    },
+  ],
+});
 
-// const usage_stats = {
-//   prompt_tokens: response.usage.prompt_tokens,
-//   completion_tokens: response.usage.completion_tokens,
-//   total_tokens: response.usage.total_tokens,
-// };
+console.log(response)
 
-// console.table(usage_stats);
+console.log(response.choices[0].message.content);
+
+
+
+const usage_stats = {
+  prompt_tokens: response.usage.prompt_tokens,
+  completion_tokens: response.usage.completion_tokens,
+  total_tokens: response.usage.total_tokens,
+};
+
+console.table(usage_stats);
--- 17 Ch--AI/05-chAI.js
+++ 17 Ch--AI/05-chAI.js
@@ -1,48 +1,48 @@
-// import { checkOpenAI } from "./01-chAI.js";
+import { checkOpenAI } from "./01-chAI.js";
 
-// const client = await checkOpenAI();
-// const model = "gpt-4o-mini";
+const client = await checkOpenAI();
+const model = "gpt-4o-mini";
 
-// console.log(client.baseURL);
+console.log(client.baseURL);
 
-// const conversation = [];
+const conversation = [];
 
-// async function askQuestion(systemPrompt, userPrompt, history = []) {
-//   const response = await client.chat.completions.create({
-//     model,
-//     messages: [
-//       { role: "system", content: systemPrompt },
-//       ...history,
-//       { role: "user", content: userPrompt },
-//     ],
-//   });
-//   history.push({ role: "user", content: userPrompt });
-//   history.push({
-//     role: "assistant",
-//     content: response.choices[0].message.content,
-//   });
-//   return response.choices[0].message.content;
-// }
+async function askQuestion(systemPrompt, userPrompt, history = []) {
+  const response = await client.chat.completions.create({
+    model,
+    messages: [
+      { role: "system", content: systemPrompt },
+      ...history,
+      { role: "user", content: userPrompt },
+    ],
+  });
+  history.push({ role: "user", content: userPrompt });
+  history.push({
+    role: "assistant",
+    content: response.choices[0].message.content,
+  });
+  return response.choices[0].message.content;
+}
 
-// const userQuestion =
-//   "My name is Hitesh and I make coding videos on YouTube, tell me a 1 line joke";
+const userQuestion =
+  "My name is Hitesh and I make coding videos on YouTube, tell me a 1 line joke";
 
-// const response1 = await askQuestion(
-//   "You always respond in 1 line",
-//   userQuestion,
-//   conversation,
-// );
+const response1 = await askQuestion(
+  "You always respond in 1 line",
+  userQuestion,
+  conversation,
+);
 
-// console.log("++++++++++ Response 1: ++++++++++");
-// console.log(response1);
+console.log("++++++++++ Response 1: ++++++++++");
+console.log(response1);
 
-// const userQuestion2 = "Tell me my Name";
+const userQuestion2 = "Tell me my Name";
 
-// const response2 = await askQuestion(
-//   "You always respond in 1 line",
-//   userQuestion2,
-//   conversation,
-// );
+const response2 = await askQuestion(
+  "You always respond in 1 line",
+  userQuestion2,
+  conversation,
+);
 
-// console.log("++++++++++ Response 2: ++++++++++");
-// console.log(response2);
+console.log("++++++++++ Response 2: ++++++++++");
+console.log(response2);
